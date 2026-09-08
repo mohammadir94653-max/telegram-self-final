b31b352fe12847177bad820e9249259c5ef2f925
mohammadir94653-max <mohammad.ir94653@gmail.com>
2026-09-08 16:32:03 +0330

    Update Information.py

--- lib/Information.py
+++ lib/Information.py
@@ -4,7 +4,7 @@ admin_user_id = 7268478044  # <--- آیدی عددی خودتان را اینج�
 api_id = 33684592  # <--- api_id خود را اینجا وارد کنید
 api_hash = '4ca8a596e43a3309f3f4cd04d427d3a1'  # <--- api_hash خود را اینجا وارد کنید
 helper_username = 'helperbotforselfbot'  # <--- یوزرنیم ربات هلپر (بدون @)
-bot_token = 8909112757:AAEHwzZB9qYnSX-G7-p8b1-7GM_rFB_tH0M''  # <--- توکن ربات هلپر
+bot_token = '8909112757:AAEHwzZB9qYnSX-G7-p8b1-7GM_rFB_tH0M'  # <--- توکن ربات هلپر
 
 client_id = '01e7dc6b41c3471b94efe87abeb05919'
 client_secret = '4f5f93af1ced4b0d9ba8440606803639'
